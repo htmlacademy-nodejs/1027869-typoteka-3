bf2f10bd40fe2fac5c532c2653224ae763fc1b0d
sergey talyzin <sergeytalyzin@MacBook-Pro-sergey.local>
2020-11-22 17:03:56 +0300

    m3-task2

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
@@ -12,5 +13,12 @@ app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 app.use(`/`, mainRoutes);
 
+const PUBLIC_DIR = `public`;
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use((req, res) => res.status(400).render(`errors/404`));
+app.use((req, res) => res.status(500).render(`errors/500`));
 
 app.listen(DEFAULT_PORT);
